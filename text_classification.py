90aebe8067b876ab2d8816a1196f2195f51d261c
Benoît Nouyrigat <benoit.nouyrigat@zenika.com>
2022-03-23 23:11:54 +0100

    Training script with requirements

---
+++ text_classification.py
@@ -0,0 +1,156 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import re
+import shutil
+import string
+import tensorflow as tf
+
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix
+
+project_path = os.path.dirname(os.path.realpath(__file__))
+
+# ## Sentiment analysis
+#
+# This notebook trains a sentiment analysis model to classify movie reviews as *positive* or *negative*, based on the text of the review. This is an example of *binary*—or two-class—classification, an important and widely applicable kind of machine learning problem.
+#
+# You'll use the [Large Movie Review Dataset](https://ai.stanford.edu/~amaas/data/sentiment/) that contains the text of 50,000 movie reviews from the [Internet Movie Database](https://www.imdb.com/). These are split into 25,000 reviews for training and 25,000 reviews for testing. The training and testing sets are *balanced*, meaning they contain an equal number of positive and negative reviews.
+#
+
+# Let's download and extract the dataset, then explore the directory structure.
+url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+
+print("### Download and extract the dataset")
+dataset = tf.keras.utils.get_file("aclImdb_v1", url,
+                                  untar=True, cache_dir='.',
+                                  cache_subdir='')
+
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+train_dir = os.path.join(dataset_dir, 'train')
+
+# ### Load the dataset
+#
+# ```
+# main_directory/
+# ...class_a/
+# ......a_text_1.txt
+# ......a_text_2.txt
+# ...class_b/
+# ......b_text_1.txt
+# ......b_text_2.txt
+# ```
+
+# To prepare a dataset for binary classification, you will need two folders on disk, corresponding to `class_a` and `class_b`. These will be the positive and negative movie reviews, which can be found in  `aclImdb/train/pos` and `aclImdb/train/neg`. As the IMDB dataset contains additional folders, you will remove them before using this utility.
+
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
+
+# Next, you will use the `text_dataset_from_directory` utility to create a labeled `tf.data.Dataset`. [tf.data](https://www.tensorflow.org/guide/data) is a powerful collection of tools for working with data.
+
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed)
+
+raw_val_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed)
+
+raw_test_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/test',
+    batch_size=batch_size)
+
+
+# ### Prepare the dataset for training
+#
+# Next, you will standardize, tokenize, and vectorize the data using the helpful `tf.keras.layers.TextVectorization` layer.
+
+
+def custom_standardization(input_data):
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+    return tf.strings.regex_replace(stripped_html,
+                                    '[%s]' % re.escape(string.punctuation),
+                                    '')
+
+
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = tf.keras.layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+
+# Make a text-only dataset (without labels), then call adapt
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+
+# Let's create a function to see the result of using this layer to preprocess some data.
+def vectorize_text(text, label):
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer(text), label
+
+
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+print('\n### Create the model')
+embedding_dim = 16
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(max_features + 1, embedding_dim),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1)])
+
+model.summary()
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer='adam',
+              metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
+
+print("\n### Train the model")
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs)
+
+print("\n### Evaluate the model")
+loss, accuracy = model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
+print("Write file metrics.txt")
+with open("metrics.txt", "w") as outfile:
+    outfile.write("Accuracy: " + str(accuracy) + "\n")
+
+threshold = .5
+y_pred = model.predict(test_ds.map(lambda x, y: x)).reshape((-1))
+y_pred = tf.math.greater(y_pred, threshold)
+y_pred = tf.cast(y_pred, dtype=tf.float32)
+y_true = np.concatenate(list(test_ds.map(lambda x, y: y)), axis=0)
+
+print("Plot confusion matrix plot.png")
+cm = confusion_matrix(y_true, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["negative", "positive"])
+disp.plot(cmap=plt.cm.Blues)
+plt.savefig("plot.png")
+
+# This fairly naive approach achieves an accuracy of about 86%.
